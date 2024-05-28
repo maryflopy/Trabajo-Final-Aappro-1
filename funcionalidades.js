87ece45c98abc36863730ad893b277b5d56ce803
florencia <166770245+maryflopy@users.noreply.github.com>
2024-05-28 02:50:59 -0300

    subida repositorio entrega trabajo final
    no me deja subir los archivos. Me dice que no tengo señal de git

---
+++ funcionalidades.js
@@ -0,0 +1,30 @@
+var nombreIngresado= prompt ("Ingrese su nombre")
+
+function saludo(nombre)
+{
+    alert("Bienvenido " + nombre);   
+}
+var nombreIngresado;
+
+while(nombreIngresado == '')
+{
+ nombreIngresado = prompt("Ingrese su nombre");
+}
+
+saludo(nombreIngresado);
+saludo
+if(nombreIngresado != ""){
+} else {
+        alert("No ingreso su nombre")
+}
+
+var edad = prompt("Ingrese su edad");
+
+const mayorEdad = 0 ;
+if(mayorEdad >= 15) {
+    console.log ("Es mayor de edad, puede realizarse un servicio");
+} else {
+    console.log("Es menor de edad, no puede realizarse ningun servicio");
+}
+var servicio = prompt ("Es su primera vez en realizarse un servicio")
+
